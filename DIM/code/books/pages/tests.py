d801ecc0d543a8fd0511522088a742af5c49e03d
Mankz111 <mgacp199@gmail.com>
2025-12-03 14:53:13 +0000

    ch4

--- DIM/code/books/pages/tests.py
+++ DIM/code/books/pages/tests.py
@@ -1,9 +1,28 @@
 from django.test import TestCase
 from django.test import SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from .views import HomePageView
 
 class HomePageTests(SimpleTestCase):
-    def home_test()
+    def setUp(self):
+        url = reverse("home")
+        self.response = self.client.get(url)
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+    def test_url_status_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(self.response.status_code, 200)
+    def test_template_usage(self):
+        self.assertTemplateUsed(self.response, 'home.html')
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'Hi there! I should not be on this page! ')
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+        )
+
 
 
 
